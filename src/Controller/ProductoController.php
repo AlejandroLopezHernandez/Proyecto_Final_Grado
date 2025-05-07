397fbebc32fbc430903b2ab4bbcead13ea0e9f06
Alejandro Lopez <alejal07@ucm.es>
2025-05-07 17:02:09 +0200

    Subiendo los cambios de nombres en archivos y las plantillas de producto

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Producto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +14,18 @@ final class ProductoController extends AbstractController
     {
         return $this->render('producto/index.html.twig', [
             'controller_name' => 'ProductoController',
+        ]);
+    }
+    #[Route('/producto/detalle', name: 'productoInterfaz')]
+    public function CardProduct(): Response
+    {
+        return $this->render('producto/plantillaProducto.html.twig');
+    }
+    #[Route('/producto/detalle/{id}', name: 'productoDetalle')]
+    public function ProductoDetalle(Producto $producto): Response
+    {
+        return $this->render('producto/cardProductoDetalle.html.twig', [
+            'producto' => $producto
         ]);
     }
 }
